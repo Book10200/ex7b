0ef0459002ad893504622858d8a49e5793f1fb2c
Sahachat <booklove132rb@gmail.com>
2025-03-05 18:33:53 +0700

    Bug fix#3 – revise candidate no. validation

--- validate.js
+++ validate.js
@@ -5,30 +5,31 @@ function checkSID() {
 	} else {
 	  return false;
 	}
-      }
-       
+        }
+         
         function checkCandiNo() {
 	let candi = (document.getElementById("candi").value).trim();
-	if (/^\d+$/.test(candi) && parseInt(candi, 10) > 0) {
+	let candiNum = parseInt(candi, 10);
+	if (/^\d+$/.test(candi) && candiNum >= 1 && candiNum <= 10) {
 	  return true;
 	} else {
 	  return false;
 	}
-      }
-       
+        }
+         
         function validateForm(){
 	if(!checkSID()){
 	  alert("Invalid value for Student ID! It must be a 10-digit number.");
 	  document.getElementById("sid").focus();
 	  return false;
-      } else {
-	if(!checkCandiNo()){
-	    alert("Invalid value for Candidate No! It must be a positive number.");
+	} else {
+	  if(!checkCandiNo()){
+	    alert("Invalid value for Candidate No! It must be a number between 1 and 10.");
 	    document.getElementById("candi").focus();
 	    return false;
-	} else {
+	  } else {
 	    alert("Your input data passes validation!!");
 	    return true;
+	  }
 	}
-      }
-      }
+        }
